b8c3bdd457d3c81ce4e59a95b7e076c5be861844
eduardo silva <eduardofla800@gmail.com>
2026-07-29 20:09:43 -0300

    Add files via upload
    Salvei a lista de compras final em um arquivo.txt

--- lista de compras.py
+++ lista de compras.py
@@ -1,6 +1,7 @@
 lista_de_compras = []
 
 while True:
+    
     print(
 '''    ==OPCOES==
 1- adicionar item
@@ -17,57 +18,76 @@ while True:
         pergunte = input('o que vc deseja adicionar? - ').lower().strip()
         
         if pergunte.strip() == '':
+            
             print('voce digitou nenhum item\n')
             
         elif pergunte in lista_de_compras:
+            
             print(f'o item {pergunte} ja esta na lista\n')
     
         else:
+            
             lista_de_compras.append(pergunte)
+            
             print(f'{pergunte} adicionado na lista\n')
         
     
     elif pergunta == '2':
+        
         print('sua lista de compras:\n')
         
         for item in lista_de_compras:
+            
             print(f'item: {item}')
+        
         pergunta = input('o que vc quer remover? - ').strip().lower()
         
         if pergunta not in lista_de_compras:
+            
             print(f'o item {pergunta} nao esta na lista\n')
         
         else:
             print('item removido\n')
+            
             lista_de_compras.remove(pergunta)
     
     elif pergunta == '3':
+        
         print('sua lista de compras:\n')
         
         print('lista de compra:\n')
         
         if len(lista_de_compras) == 0:
+            
             print('a lista esta vazia\n')
             
         else:
         
             for item in lista_de_compras:
+                
                 print(f'item: {item}\n')
             
     elif pergunta == '4':
+        
         print('saindo...\n')
+        
         break
         
     else:
+        
         print('digite uma opcao valida, EX: 1, 2, 3 ou 4\n')
         
 print('lista completa!\n')
 print('lista:\n')
 
 if  len(lista_de_compras) == 0:
+    
     print('vazia')
 
-else:
-    
-    for item in lista_de_compras:
-        print(f'item: {item}')
+else:     
+       
+       with open('compras.txt', 'a') as a:
+           
+           for item in lista_de_compras:
+               a.write(item)
+               print(f'item{item}')
